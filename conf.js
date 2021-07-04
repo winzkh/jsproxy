f6febf95d8517e8409bfbb05d235a7c9452bd2d9
winzkh <55969272+winzkh@users.noreply.github.com>
2021-07-04 16:58:33 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,7 +29,7 @@ jsproxy_config({
     'mysite': {
       label: '当前站点',
       lines: {
-        [location.host]: 1,
+        'qcloud-sg-01.proxy.nnsl.xyz': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
